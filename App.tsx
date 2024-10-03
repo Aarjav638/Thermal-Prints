7cc44e11bc630aef552b938e08d6766e363b2c4d
Aarjav Jain <anshjain638@gmail.com>
2024-10-03 17:02:49 +0530

    Use thermal printer package for printing over the network and bluetooth

--- App.tsx
+++ App.tsx
@@ -1,118 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {View, NativeModules, Button, Alert} from 'react-native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import ThermalPrinterModule from 'react-native-thermal-printer';
+import {useNetInfo} from '@react-native-community/netinfo';
+const App = () => {
+  const [ipAddress, setIpAddress] = React.useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  OneSignal.initialize('b51a4c39-d728-4338-b117-6435fd2b3137');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  OneSignal.Notifications.requestPermission(true);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const {CustomModule} = NativeModules;
+  const netInfo = useNetInfo();
+  const getIpAddress = React.useCallback(() => {
+    try {
+      if (netInfo.details && 'ipAddress' in netInfo.details) {
+        setIpAddress(netInfo.details.ipAddress as string);
+      } else {
+        console.log('ipAddress not available');
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  }, [netInfo.details]);
+
+  React.useEffect(() => {
+    getIpAddress();
+  }, [getIpAddress, netInfo.isConnected]);
+
+  const onpress = async () => {
+    try {
+      const res = await CustomModule.createEvent('Testing', 'Test Location');
+      Alert.alert(
+        'Response',
+        `Event Name ${res.name} location ${res.location}`,
+        [{text: 'OK'}],
+      );
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const handleNetworkPrint = async () => {
+    console.log('ipAddress', ipAddress);
+    ThermalPrinterModule.defaultConfig.ip = ipAddress;
+    ThermalPrinterModule.defaultConfig.port = 9100;
+    ThermalPrinterModule.defaultConfig.autoCut = false;
+    ThermalPrinterModule.defaultConfig.timeout = 30000;
+    try {
+      console.log('Printing...');
+      await ThermalPrinterModule.printTcp({payload: 'hello world'});
+    } catch (err) {
+      console.log((err as any).message);
+    }
+  };
+
+  const handleBluetoothPrint = async () => {
+    try {
+      console.log('Printing...');
+      await ThermalPrinterModule.printBluetooth({payload: 'hello world'});
+    } catch (err) {
+      console.log((err as any).message);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        gap: 15,
+        alignItems: 'center',
+      }}>
+      <Button title="Invoke Native Module" onPress={onpress} />
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+      <Button
+        title="Print Using Network Printer"
+        onPress={handleNetworkPrint}
+      />
+      <Button
+        title="Print Using Bluetooth Printer"
+        onPress={handleBluetoothPrint}
+      />
+    </View>
+  );
+};
 
 export default App;
